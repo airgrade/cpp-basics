94b7871d0dd1ab8bbadfe3e4d3088de74a7998d6
airgrade <charlesmugendi2020@gmail.com>
2024-12-13 20:00:57 +0300

    my third c++ code four lines

---
+++ code3.cpp
@@ -0,0 +1,17 @@
+//simple four lines of code that displays text, including the sum of numbers.
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int num = 6;
+
+    cout << "My third C++ program. " << endl;
+    cout << "The sum of 2 and 3 = " << 5 << endl;
+    cout << "7 + 8 =" << 7 + 8 << endl;
+    cout << "Num =" << num << endl;
+
+    return 0;
+}

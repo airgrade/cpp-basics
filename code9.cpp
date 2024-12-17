ac0f47ebbf7255a03cffc4d2310cff49a0a75917
airgrade <charlesmugendi2020@gmail.com>
2024-12-17 21:21:38 +0300

    average of three game scores

---
+++ code9.cpp
@@ -0,0 +1,33 @@
+// program that calculates the average scores of player
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main(){
+
+    // varibles
+    int firstScore, secondScore, thirdScore;
+
+    //user response
+    cout << "Enter your first  gaming score: ";
+    cin >> firstScore;
+
+    cout << "Enter your second gaming score: ";
+    cin >> secondScore;
+
+    cout << "Enter your third gaming score: ";
+    cin >> thirdScore;
+
+    // avg score calculation
+    double avgScore = (firstScore + secondScore + thirdScore) / 3;
+
+    cout << "Your average score is: " << avgScore <<endl;
+
+    return 0;
+}
+

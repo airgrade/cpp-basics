51656955e48c2588930e0990157ae9be31b02796
airgrade <charlesmugendi2020@gmail.com>
2024-12-19 13:41:43 +0300

    used switch fuction

---
+++ code13.cpp
@@ -0,0 +1,36 @@
+// Menu chooser
+// Using switch statements
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Difficulty Levels \n\n";
+    cout << "1- Easy \n";
+    cout << "2- Normal \n";
+    cout << "3- Hard \n\n";
+
+    int choice;
+    cout << "Choose Level: ";
+    cin >> choice;
+
+    switch(choice){
+
+        case 1:
+                cout << "You picked easy.\n";
+                break;
+        case 2:
+                cout << "You picked Normal. \n";
+                break;
+        case 3: 
+                cout << "You picked Hard. \n";
+                break;
+        default:
+                cout << "You made an illegal choice.\n";
+
+
+    }
+
+    return 0;
+}

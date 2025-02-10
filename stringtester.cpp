2b30571f4d6075f0e2893a5aa83d1daad2716d09
airgrade <charlesmugendi2020@gmail.com>
2025-02-10 22:30:39 +0300

    changing position of character

--- stringtester.cpp
+++ stringtester.cpp
@@ -9,8 +9,17 @@ int main(){
 
     string word1 = "Game";
     string word2 ("Over");
-    string word3 (3,"!");
+    string word3 (3,'!');
 
     string phrase = word1 + " " + word2 + word3;
     cout << "The phrase is: " << phrase << "\n\n";
+
+    cout << "The character at position 0 is: " << phrase [0] << "\n\n";
+
+    cout << "Changing the character at position 0. \n"; 
+
+    phrase[0] = 'L';
+
+    cout << "The phrase is now: " << phrase << "\n\n";
+
 }

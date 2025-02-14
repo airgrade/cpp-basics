edee1d361f608ea128e665073d56d7e99b192cb0
airgrade <charlesmugendi2020@gmail.com>
2025-02-14 23:19:01 +0300

    used string member fuction size()

--- array1.cpp
+++ array1.cpp
@@ -23,5 +23,16 @@ int main(){
 
     cout << "\nYou trade your sword for a battle axe.\n";
     cout << "The first item on the list is: "<< inventory[0] << endl;
+    // change the first item to battle axe after the trade 
+
+    inventory[0] = "battle axe";
+    cout << "\nThe items available in the inventory\n";
+    int numbering = 0;
+    for (int i = 0; i < numItems; ++i){
+        ++numbering;
+        cout << numbering<< ". " << inventory[i]<< endl;
+    }
+    cout << "\nThe item name '" << inventory[0] << "' has ";
+    cout << inventory[0].size() << " letters in it.\n";
 
 }

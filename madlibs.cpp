0e5a5e1f0be884f35ffe739c032b199850951e3f
airgrade <charlesmugendi2020@gmail.com>
2025-02-21 11:51:30 +0300

    wrote the first function for the program

---
+++ madlibs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+//instructions on how to play the game
+//also displays the first time i create functions in c++
+
+using namespace std;
+
+//function prototype (declaration)
+void instrunctions();
+
+int main()
+{
+    instrunctions();
+    return 0;
+}
+
+//function definition
+void instrunctions()
+{
+    cout << "Welcome to the most fun you've ever had with text!\n\n";
+    cout << "Here is how to play the game......\n";
+}

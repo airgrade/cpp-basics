0e3c69c51217c0c0228d26c69f1e5b1805d80583
airgrade <charlesmugendi2020@gmail.com>
2025-03-04 21:51:21 +0300

    default parameters in a function

---
+++ give_me_a_number.cpp
@@ -0,0 +1,29 @@
+//Program to demonstrate default function arguements
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int askNumber(int high, int low = 1);
+
+int main()
+{
+    int number = askNumber(5);
+    cout << "Thanks for entering: " << number << "\n\n";
+
+    number = askNumber(10, 5);
+    return 0;
+}
+
+int askNumber(int high, int low)
+{
+    int num;
+    do
+    {
+       cout << "Please enter a number" << "(" << low << " - " << high << "): ";
+       cin >> num;
+
+    } while (num > high || num < low);
+    
+    return num;
+}

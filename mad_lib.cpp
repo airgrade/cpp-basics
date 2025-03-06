b99d7ffabdffe9bcabf49fc2a9483cc7056a24a0
airgrade <charlesmugendi2020@gmail.com>
2025-03-06 22:59:00 +0300

    created file and function prototype for madlib

---
+++ mad_lib.cpp
@@ -0,0 +1,12 @@
+//mad lib story based on user input
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string askText(string prompt);
+int askNumber(string prompt);
+void tellStory(string name, string noun, int number, string bodyPart, string verb);
+
+
